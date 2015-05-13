28002b988836d7cba2f12d421741e76a38511eed
khpolash <khasan994@gmail.com>
2015-05-13 16:46:40 +0600

    Initial CircleOperationApp

---
+++ CircleOperationApp/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace CircleOperationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radiusTextBox = new System.Windows.Forms.TextBox();
+            this.resultButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Radius of Circle";
+            // 
+            // radiusTextBox
+            // 
+            this.radiusTextBox.Location = new System.Drawing.Point(164, 41);
+            this.radiusTextBox.Name = "radiusTextBox";
+            this.radiusTextBox.Size = new System.Drawing.Size(238, 20);
+            this.radiusTextBox.TabIndex = 1;
+            // 
+            // resultButton
+            // 
+            this.resultButton.Location = new System.Drawing.Point(196, 87);
+            this.resultButton.Name = "resultButton";
+            this.resultButton.Size = new System.Drawing.Size(175, 23);
+            this.resultButton.TabIndex = 5;
+            this.resultButton.Text = "Result";
+            this.resultButton.UseVisualStyleBackColor = true;
+            this.resultButton.Click += new System.EventHandler(this.resultButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 160);
+            this.Controls.Add(this.resultButton);
+            this.Controls.Add(this.radiusTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Circle Operation UI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox radiusTextBox;
+        private System.Windows.Forms.Button resultButton;
+    }
+}
+
---
+++ CircleOperationApp/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircleOperationApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Circle aCircle = new Circle();
+        private void resultButton_Click(object sender, EventArgs e)
+        {
+            if (radiusTextBox.Text !="")
+            {
+                if (Convert.ToDouble(radiusTextBox.Text)>0)
+                {
+                    aCircle.radius = Convert.ToDouble(radiusTextBox.Text);
+                    MessageBox.Show("Area : " + aCircle.GetArea() + "\nDiameter : " + aCircle.GetDiameter() + "\nPerimeter : " + aCircle.GetPerimeter());
+                }
+                else
+                {
+                    MessageBox.Show("Circle does not exist any negetive value...!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter radius..........!");
+            }
+            radiusTextBox.Clear();
+        }
+    }
+}
